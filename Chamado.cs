ef602c514bac09f4e1cf60758ca9ef444fd7c6db
João Teixeira <joao.teixeira@ifro.edu.br>
2025-08-26 22:12:08 -0400

    Aula - 26/08

---
+++ Chamado.cs
@@ -0,0 +1,13 @@
+﻿namespace ApiServico
+{
+    public class Chamado
+    {
+        public int Id { get; set; }
+
+        public required string Titulo { get; set; }
+
+        public required string Descricao { get; set; }
+
+        public string Status { get; set; } = "Aberto";
+    }
+}
---
+++ Controllers/ChamadoController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiServico.Controllers
+{
+    [Route("/chamados")]
+    [ApiController]
+    public class ChamadoController : ControllerBase
+    {
+
+        private static List<Chamado> _listaChamados = new List<Chamado>
+        {
+            new Chamado() { 
+                Id = 1, Titulo = "Erro na Tela de Acesso", Descricao = "O usuário não conseguiu logar" },
+            new Chamado() { 
+                Id = 2, Titulo = "Sistema com lentidão", Descricao = "Demora no carregamento das telas"}
+        };
+
+        private static int _proximoId = 3;
+
+        [HttpGet]
+        public IActionResult BuscarTodos()
+        {
+            return Ok(_listaChamados);
+        }
+
+        [HttpPost]
+        public IActionResult Criar([FromBody] Chamado novoChamado)
+        {
+            novoChamado.Id = _proximoId++;
+            novoChamado.Status = "Aberto";
+
+            _listaChamados.Add(novoChamado);
+
+            return Created("", novoChamado);
+        }
+    }
+}
